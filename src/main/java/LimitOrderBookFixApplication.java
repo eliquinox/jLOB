93d0be2a85c9a42ce311277e19c84c7b81eb5b41
Eduard Silantyev <eduard.silantyev@revolut.com>
2020-02-17 17:52:21 +0000

    Remove redundancies

--- src/main/java/LimitOrderBookFixApplication.java
+++ src/main/java/LimitOrderBookFixApplication.java
@@ -1,11 +1,8 @@
-import quickfix.ConfigError;
-import quickfix.SessionNotFound;
-
 import static connectivity.fix.FixClient.clientRunnable;
 import static connectivity.fix.FixServer.serverRunnable;
 
 public class LimitOrderBookFixApplication {
-    public static void main(String[] args) throws ConfigError, InterruptedException, SessionNotFound {
+    public static void main(String[] args) {
         new Thread(serverRunnable).start();
         new Thread(clientRunnable).start();
     }
