b940dd53129ff8a4016d8409d7bd1b75035a9448
eliquinox <eliquinox@outlook.com>
2020-06-07 01:18:06 +0100

    FMT

--- src/main/java/connectivity/LimitOrderBookHttpServerRunner.java
+++ src/main/java/connectivity/LimitOrderBookHttpServerRunner.java
@@ -27,22 +27,21 @@ public class LimitOrderBookHttpServerRunner implements ServerRunner {
         post("vwap", limitOrderBookApi.getVwap);
         before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
         options("/*", (request, response) -> {
+            String accessControlRequestHeaders = request
+                    .headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers",
+                        accessControlRequestHeaders);
+            }
 
-                    String accessControlRequestHeaders = request
-                            .headers("Access-Control-Request-Headers");
-                    if (accessControlRequestHeaders != null) {
-                        response.header("Access-Control-Allow-Headers",
-                                accessControlRequestHeaders);
-                    }
+            String accessControlRequestMethod = request
+                    .headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods",
+                        accessControlRequestMethod);
+            }
 
-                    String accessControlRequestMethod = request
-                            .headers("Access-Control-Request-Method");
-                    if (accessControlRequestMethod != null) {
-                        response.header("Access-Control-Allow-Methods",
-                                accessControlRequestMethod);
-                    }
-
-                    return "OK";
+            return "OK";
         });
     }
 }
