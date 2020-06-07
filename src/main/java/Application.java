2350e4187eca55dce5d3cb57e99300b26906fb5c
eliquinox <eliquinox@outlook.com>
2020-06-07 03:58:01 +0100

    log exception cause

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -7,9 +7,13 @@ import db.Migrator;
 
 public class Application {
     public static void main(String[] args)  {
-        Injector injector = Guice.createInjector(new ApplicationModule(args[0]));
-        injector.getInstance(Migrator.class).migrate();
-        injector.getInstance(LimitOrderBookHttpServerRunner.class).run();
-        injector.getInstance(LimitOrderBookFixServerRunner.class).run();
+        try {
+            Injector injector = Guice.createInjector(new ApplicationModule(args[0]));
+            injector.getInstance(Migrator.class).migrate();
+            injector.getInstance(LimitOrderBookHttpServerRunner.class).run();
+            injector.getInstance(LimitOrderBookFixServerRunner.class).run();
+        } catch (Exception e) {
+            System.out.println(e.getCause());
+        }
     }
 }
