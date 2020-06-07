b500a7cd3a3dac39bcf4f609ea06d063301eb047
eliquinox <eliquinox@outlook.com>
2020-06-07 01:16:25 +0100

    CORS

--- src/main/java/connectivity/LimitOrderBookHttpServerRunner.java
+++ src/main/java/connectivity/LimitOrderBookHttpServerRunner.java
@@ -4,10 +4,7 @@ import com.google.inject.Inject;
 import connectivity.http.LimitOrderBookApi;
 import state.LimitOrderBook;
 
-import static spark.Spark.path;
-import static spark.Spark.get;
-import static spark.Spark.post;
-import static spark.Spark.put;
+import static spark.Spark.*;
 
 
 public class LimitOrderBookHttpServerRunner implements ServerRunner {
@@ -22,11 +19,30 @@ public class LimitOrderBookHttpServerRunner implements ServerRunner {
 
     @Override
     public void run() {
-        path("book", () -> {
+        path("/book", () -> {
             get(ORIGIN, limitOrderBookApi.getLimitOrderBook);
             post(ORIGIN, limitOrderBookApi.placeOrder);
             put(ORIGIN, limitOrderBookApi.reduceOrder);
         });
         post("vwap", limitOrderBookApi.getVwap);
+        before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
+        options("/*", (request, response) -> {
+
+                    String accessControlRequestHeaders = request
+                            .headers("Access-Control-Request-Headers");
+                    if (accessControlRequestHeaders != null) {
+                        response.header("Access-Control-Allow-Headers",
+                                accessControlRequestHeaders);
+                    }
+
+                    String accessControlRequestMethod = request
+                            .headers("Access-Control-Request-Method");
+                    if (accessControlRequestMethod != null) {
+                        response.header("Access-Control-Allow-Methods",
+                                accessControlRequestMethod);
+                    }
+
+                    return "OK";
+        });
     }
 }
